9eeb7470fc18b9d71eef1b1715fc3322d932eb4d
DamnLow <qmzhollow@googlemail.com>
2021-02-02 17:46:34 +0100

    noch ein test

--- eda-lab/implementation/Analysis-EDA/AnalysisEDA.cpp
+++ eda-lab/implementation/Analysis-EDA/AnalysisEDA.cpp
@@ -12,7 +12,7 @@ void AnalysisEDA::run() {
     //std::cout << __PRETTY_FUNCTION__ << " is not implemented!" << std::endl;
     // Todo: Implement the simulator here
 
-    //test for git, test von DamnLow
+    //test for git, noch ein test
     // Iterate all elements:
     for (const Element* element : graphHandler->getAllElements()) {
         std::cout << element->getName() << std::endl;
